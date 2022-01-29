342a0ce39aea12e8ac0b089bf95102d110963d13
rimprog <riminprog@gmail.com>
2022-01-29 22:00:54 +0300

    Redistributed functions into modules

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,13 +1,8 @@
-import copy
-
 from django import forms
-from django.conf import settings
 from django.shortcuts import redirect, render
 from django.views import View
 from django.urls import reverse_lazy
-from django.utils import timezone
 from django.contrib.auth.decorators import user_passes_test
-from django.core.exceptions import FieldError
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
@@ -18,9 +13,9 @@ from foodcartapp.models import RestaurantMenuItem
 from foodcartapp.models import Order
 
 from geocoderapp.models import Place
-
-import requests
-from geopy import distance
+from geocoderapp.views import find_not_created_places_for_items_with_addresses
+from geocoderapp.views import bulk_create_places_by_addresses
+from geocoderapp.views import add_distance_to_restaurant
 
 
 class Login(forms.Form):
@@ -105,99 +100,6 @@ def view_restaurants(request):
     return render(request, template_name="restaurants_list.html", context={
         'restaurants': Restaurant.objects.all(),
     })
-
-
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lat, lon
-
-
-def fetch_place(address):
-    place_coordinates = fetch_coordinates(
-        settings.YANDEX_GEOCODER_TOKEN,
-        address
-    )
-
-    if place_coordinates:
-        lat, lon = place_coordinates
-        place = Place(
-            address=address,
-            latitude=lat,
-            longitude=lon,
-            refreshed_at=timezone.now()
-        )
-
-        return place
-
-
-def find_not_created_places_for_items_with_addresses(items):
-    places_addresses = Place.objects.values('address')
-
-    try:
-        not_created_places_for_items = items.exclude(address__in=places_addresses)
-        not_created_places_addresses = not_created_places_for_items.values('address').distinct()
-    except FieldError:
-        not_created_places_for_items = items.exclude(restaurant__address__in=places_addresses)
-        not_created_places_addresses = not_created_places_for_items.values('restaurant__address').distinct()
-
-    return not_created_places_addresses
-
-
-def bulk_create_places_by_addresses(place_addresses):
-    places = []
-    for place_address in place_addresses:
-        try:
-            place = fetch_place(place_address['address'])
-        except KeyError:
-            place = fetch_place(place_address['restaurant__address'])
-
-        if place:
-            places.append(place)
-
-    created_places = Place.objects.bulk_create(places)
-
-    return created_places
-
-
-def add_distance_to_restaurant(restaurants, order_coordinates, places):
-    restaurants_with_distances = []
-
-    for restaurant in restaurants:
-        place = list(
-            filter(
-                lambda place: place.address == restaurant.address,
-                places
-            )
-        )[0]
-        restaurant_coordinates = (place.latitude, place.longitude)
-
-        restaurant.distance_to_order_address = round(
-            distance.distance(restaurant_coordinates, order_coordinates).km, 3
-        )
-
-        restaurant_copy = copy.copy(restaurant)
-
-        restaurants_with_distances.append(restaurant_copy)
-
-    sorted_restaurants_with_distances = sorted(
-        restaurants_with_distances,
-        key=lambda restaurant: restaurant.distance_to_order_address
-    )
-
-    return sorted_restaurants_with_distances
 
 
 def find_restaurants_that_can_prepare_order(order, restaurant_menu_items):
