a25f0c31a21d821bbc0f397ba4fa7026417f2068
rimprog <riminprog@gmail.com>
2022-04-08 11:37:22 +0300

    Bug fix for empty product category

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -54,7 +54,7 @@ def product_list_api(request):
             'category': {
                 'id': product.category.id,
                 'name': product.category.name,
-            },
+            } if product.category else None,
             'image': product.image.url,
             'restaurant': {
                 'id': product.id,
